e87a8a42db58c92ce0443f3bf08042af751c47b4
Vincent-GL <vincent.goguyer-lalande@epita.fr>
2021-02-26 11:18:22 +0100

    début de script
    un début de script pour le multijoueur

---
+++ Projet_S2/Assets/Scripts/multijoueur/Launcher.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class Launcher : MonoBehaviour
+{
+    #region Private Serializable Fields
+    #endregion
+    #region Private Fields
+    string Gameversion = "1";
+    #endregion
+    #region MonoBehaviour CallBacks
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+    void Start()
+    {
+        
+    }
+    #endregion
+    #region Public Methods
+    public void Connect()
+    {
+        if(PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = Gameversion;
+        }
+    }
+    #endregion
+}
