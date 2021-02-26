e19b150627a3940b557f115b52725a858baa941c
MaevaLasserre <78643706+MaevaLasserre@users.noreply.github.com>
2021-02-26 17:19:15 +0100

    je tente des choses

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+
+    public void Jouer()
+    {
+        SceneManager.LoadScene("Leveltuto");
+    }
+
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+}
