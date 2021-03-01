c28e437068b833c244b89b40778375d034418ece
Nathan Biteghe <nathan.biteghe@epita.fr>
2021-03-01 17:57:43 +0100

    ajout_du_perso

---
+++ zProjet/Assets/Scripts/Personnage/mouv.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouv : MonoBehaviour
+{
+    public float speed;
+    public float jumphigh;
+    public Rigidbody2D rb;
+    private bool IsGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //saut
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && IsGrounded)
+        {
+            jump();
+        }
+    }
+
+    public void jump()
+    {
+        rb.velocity += new Vector2(0, jumphigh);
+    }
+
+    //Mouvement gauche/droite
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal")*speed;
+        Vector2 rl = new Vector2(moveHorizontal, 0);
+        rb.AddForce(rl);
+    }
+
+    //Conditions de saut
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("sol"))
+        {
+            IsGrounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if(col.gameObject.CompareTag("sol"))
+        {
+            IsGrounded = false;
+        }
+    }
+}
