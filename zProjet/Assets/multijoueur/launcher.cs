4ee4677b0aad11b894feba4d8d6ef4a83ee4dc93
Vincent-GL <vincent.goguyer-lalande@epita.fr>
2021-03-02 11:20:10 +0100

    Multi
    Ajout du script laucher

--- zProjet/Assets/multijoueur/launcher.cs
+++ zProjet/Assets/multijoueur/launcher.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using Photon.Realtime;
 
-public class launcher : MonoBehaviour
+public class launcher : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    string Version = "0.1";
+
+    [SerializeField] private GameObject usernameMenu;
+    [SerializeField] private GameObject ConnectPannel; // Sert à choisir de créer ou rejoindre une partie
+    [SerializeField] private GameObject quickmatchbutton;
+
+    [SerializeField] private InputField JoinGameInput;
+    [SerializeField] private InputField CreateGameInput;
+    [SerializeField] private InputField usernameInput;
+
+    void Awake()
     {
-        
+        PhotonNetwork.GameVersion = Version;
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        usernameMenu.SetActive(true);
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("successfully connected");
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Disconnected from server : " + cause);
+    }
+    public void SetUserName()
+    {
+        usernameMenu.SetActive(false);
+        if (usernameInput.text.Length > 0)
+            PhotonNetwork.NickName = (usernameInput.text);
+        else
+        {
+            PhotonNetwork.NickName = "Anonyme";
+        }
+    }
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(CreateGameInput.text, new RoomOptions { MaxPlayers = 2 }, null);
+    }
+    public void JoinRoom()
+    {
+        RoomOptions Roptions = new RoomOptions();
+        Roptions.MaxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom(JoinGameInput.text, Roptions, TypedLobby.Default);
     }
 }
