fb6191d4f518a8e0b86f092694a2799c8e2c81fc
Nathan Biteghe <nathan.biteghe@epita.fr>
2021-03-02 17:17:31 +0100

    camera

---
+++ zProjet/Assets/Scripts/Personnage/camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+
+    public GameObject personnage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(personnage.transform.position.x, personnage.transform.position.y, -2);
+    }
+}
