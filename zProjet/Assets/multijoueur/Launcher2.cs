0975e6bd0fd1f5ac20b735f2dfe192a1fa378d81
Vincent-GL <vincent.goguyer-lalande@epita.fr>
2021-04-07 11:07:25 +0200

    Multijoueur
    Modification du script de connection : le joueur peut désormais se connecter à une room aléatoire.

---
+++ zProjet/Assets/multijoueur/Launcher2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using Photon.Realtime;
+public class Launcher2 : MonoBehaviourPunCallbacks
+{
+    string GameVersion = "0.1";
+  void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+    void Start()
+    {
+        Connect();
+    }
+
+    public void Connect()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = GameVersion;
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("successfully connected to Master");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("Disconnected for the following reason : {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("couldn't join a random room");
+        RoomOptions Roptions = new RoomOptions();
+        Roptions.MaxPlayers = 2;
+        PhotonNetwork.CreateRoom(null, Roptions);
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Successfully joined a room");
+    }
+}
