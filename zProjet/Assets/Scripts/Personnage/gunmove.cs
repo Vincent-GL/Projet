440e3c8736fff263df2a442e90b952309d6d46ff
Nathan Biteghe <nathan.biteghe@epita.fr>
2021-04-28 21:34:53 +0200

    y a un bras mtn

---
+++ zProjet/Assets/Scripts/Personnage/gunmove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunmove : MonoBehaviour
+{
+   
+    
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition)- transform.position;
+
+        float rotz = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotz);
+    }
+}
