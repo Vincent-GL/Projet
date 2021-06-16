509066c42e798022ff06bb82d0f14d5b4b0cd853
Vincent-GL <vincent.goguyer-lalande@epita.fr>
2021-06-16 10:45:29 +0200

    Tout mdr

---
+++ Projet_S2/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    public void Start()
+    {
+        resolutions = Screen.resolutions.Select(resolution => 
+        new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int index = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+                index = i;
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = index;
+        resolutionDropdown.RefreshShownValue();
+
+        Screen.fullScreen = true;
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+    
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int index)
+    {
+        Resolution resolution = resolutions[index];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
---
+++ Projet_S2/Assets/Scripts/transitionNiveau.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class transitionNiveau : MonoBehaviour
+{
+    
+    public int niveau;
+    void OnTriggerEnter2D()
+    {
+        SceneManager.LoadScene(niveau);
+    }
+    
+}
