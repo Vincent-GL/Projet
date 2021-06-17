6e3dc281eea7281a509a4685757f6df24c6886b0
Nathan Biteghe <nathan.biteghe@epita.fr>
2021-06-17 19:20:03 +0200

    animations part2

---
+++ Projet_S2/Assets/Scripts/Personnage/Anim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StateAnim
+{
+    course,
+    idle,
+    grounded
+}
+
+public class Anim : MonoBehaviour
+{
+    private Animator Animat;
+    private mouv player;
+    //private playerMotor playermotor;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
