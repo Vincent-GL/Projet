0d6e2ec640929ae371ddadc7d3bb48b73a0bb428
Nathan Biteghe <nathan.biteghe@epita.fr>
2021-06-20 17:35:01 +0200

    shoot partie 1,5

--- Projet_S2/Assets/Scripts/Personnage/Tir.cs
+++ Projet_S2/Assets/Scripts/Personnage/Tir.cs
@@ -19,6 +19,13 @@ public class Tir : MonoBehaviour
     
     public AudioClip SoundShoot;
 
+
+    private void Awake()
+    {
+        firepoint = transform.GetChild(0);
+    }
+
+
     void Update()
     {
         if (firerate == 0)
@@ -37,13 +44,16 @@ public class Tir : MonoBehaviour
     }
     private void Shoot()
     {
-        /*Vector3 mousePositionWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePositionWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
         Vector2 mousePosition = new Vector2(mousePositionWorld.x, mousePositionWorld.y);
+
         Vector2 Firepointposition = new Vector2(firepoint.position.x, firepoint.position.y);
 
         RaycastHit2D hit = Physics2D.Raycast(Firepointposition, mousePosition - Firepointposition, Mathf.Infinity, notToHit);
 
-        Debug.DrawLine(Firepointposition, (mousePosition - Firepointposition) * 100, Color.red);*/
+        Debug.DrawLine(Firepointposition, (mousePosition - Firepointposition) * 100, Color.red);
+
         Debug.Log("fire");
         GetComponent<AudioSource>().PlayOneShot(SoundShoot);
     }
--- Projet_S2/Assets/Scripts/Personnage/gunmove.cs
+++ Projet_S2/Assets/Scripts/Personnage/gunmove.cs
@@ -12,6 +12,22 @@ public class gunmove : MonoBehaviour
 
         float rotz = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
 
-        transform.rotation = Quaternion.Euler(0f, 0f, rotz);
+        //transform.rotation = Quaternion.Euler(0f, 0f, rotz-25);
+
+        float x = Input.GetAxisRaw("Horizontal");
+        if (x == -1)
+        {
+            GetComponent<SpriteRenderer>().flipY = true;
+        }
+        else if (x == 1)
+        {
+            GetComponent<SpriteRenderer>().flipY = false;
+        }
+
+        if (GetComponent<SpriteRenderer>().flipY == false)
+            transform.rotation = Quaternion.Euler(0f, 0f, rotz - 30);
+        else if (GetComponent<SpriteRenderer>().flipY == true)
+            transform.rotation = Quaternion.Euler(0f, 0f, rotz +30);
+
     }
 }
