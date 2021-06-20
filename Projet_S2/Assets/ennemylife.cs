d9c36b04b4e745d8e9c247577db472374679b42a
Nathan Biteghe <nathan.biteghe@epita.fr>
2021-06-20 21:53:07 +0200

    perso+colliders niveau 2

---
+++ Projet_S2/Assets/ennemylife.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ennemylife : MonoBehaviour
+{
+    public static float pv = 3;
+    public bool IsAlive = (pv > 1);
+
+    
+}
